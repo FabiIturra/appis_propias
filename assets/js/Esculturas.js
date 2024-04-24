92c802363d9b58b7ac529ecb98b2bb713ddaef06
FabiIturra <163338654+FabiIturra@users.noreply.github.com>
2024-04-24 13:46:43 -0400

    primer commit

---
+++ assets/js/Esculturas.js
@@ -0,0 +1,37 @@
+Esculturas =[
+    {
+        ID: "1",
+        Nombre:"alumbrador",
+        Material:"metal",
+        autor:"Marjorie",
+        Precio:"15000",
+    },
+    {
+        ID: "2",
+        Nombre:"mirador",
+        Material:"metal",
+        autor:"Marjorie",
+        Precio:"10000", 
+    },
+    {
+        ID: "3",
+        Nombre:"pescador",
+        Material:"greda",
+        autor:"Marjorie",
+        Precio:"5000",
+    },
+    {
+        ID: "4",
+        Nombre:"mental",
+        Material:"acero",
+        autor:"Marjorie",
+        Precio:"20000",
+    },
+    {
+        ID: "2",
+        Nombre:"bonzai",
+        Material:"madera",
+        autor:"Marjorie",
+        Precio:"25000",
+    },
+]
